7c09c7bf92176cf6f6432e9e7a1ff51e8db9777a
MagnusTerra <escalantejose22@gmail.com>
2023-06-10 17:26:49 -0600

    Script actualizado

--- script.sh
+++ script.sh
@@ -10,7 +10,7 @@ sudo apt-get update && sudo apt-get install -y \
 # Por si ya existe una carpeta llamada gAbrIel
 sudo virtualenv gAbriel 
 
-sudo cd gAbriel 
+cd gAbriel 
 
 sudo source bin/activate
 
