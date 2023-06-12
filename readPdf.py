1fc7ff834ae15f00c0a0a9aa8322d208bb20ed19
Satan-S-ama <omarcruz.oc827@gmail.com>
2023-06-11 19:57:52 -0600

    pruebas de la libreria PyPDF2

--- readPdf.py
+++ readPdf.py
@@ -1,2 +1,13 @@
+from PyPDF2 import PdfReader
+
+reader = PdfReader("prueba.pdf")
+page = reader.pages[0]
+prueba = open ('prueba.txt','w')
+prueba.write(page.extract_text())
+prueba.close()
+''' 
+    Agarrar todas las paginas
+    Cree un archivo con el nombre del pdf
+'''
 x = 'juan'
 print ('Angelo estuvo aqui')
