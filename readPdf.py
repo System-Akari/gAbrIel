4d4024c243c19d2d0eb094c45c17b2b2fd61aca6
Satan-S-ama <omarcruz.oc827@gmail.com>
2023-06-12 20:38:06 -0600

    Extraer texto de un pdf con 1 o mas paginas y escribirla en txt

--- readPdf.py
+++ readPdf.py
@@ -1,13 +1,16 @@
 from PyPDF2 import PdfReader
+import os
 
-reader = PdfReader("prueba.pdf")
-page = reader.pages[0]
-prueba = open ('prueba.txt','w')
-prueba.write(page.extract_text())
-prueba.close()
-''' 
-    Agarrar todas las paginas
-    Cree un archivo con el nombre del pdf
-'''
+lector = PdfReader('prueba_merged.pdf') #creamos una variable que lea el archivo
+nombre_doc_base = os.path.basename('prueba_merged.pdf') #extraemos el nombre del archivo con su extension
+nombre_doc = os.path.splitext(nombre_doc_base)[0] + '.txt' #creamos un txt que se llame como el archivo original sin su extension
+for i in range(len(lector.pages)): #ciclo for que recorre el archivo pdf pagina por pagina
+    pagina = lector.pages[i] #extraemos el texto de la pagina 'i'
+
+    #abrimos el archivo txt y escribimos el texto extraido con el lector y al final lo cerramos(buena practica)
+    archivoN = open (nombre_doc,'a') 
+    archivoN.write(pagina.extract_text()) 
+    archivoN.close()
+
 x = 'juan'
 print ('Angelo estuvo aqui')
