dcb76f75daf2024ade763f4e2052f6281618ac60
Satan-S-ama <omarcruz.oc827@gmail.com>
2023-06-21 18:52:56 -0600

    Agregando restriccion de paginas a traducir

--- readPdf.py
+++ readPdf.py
@@ -7,12 +7,14 @@ def pdf_a_txt(archivoPDF):
     nombre_doc_base = os.path.basename(archivoPDF) #extraemos el nombre del archivo con su extension
     nombre_doc = os.path.splitext(nombre_doc_base)[0] + '.txt' #creamos un txt que se llame como el archivo original sin su extension
     for i in range(len(lector.pages)): #ciclo for que recorre el archivo pdf pagina por pagina
-        pagina = lector.pages[i] #extraemos el texto de la pagina 'i'
+        cont = cont + 1 #contador que restringe la cantidad de paginas a traducir
+        if cont >= 50:
+            pagina = lector.pages[i] #extraemos el texto de la pagina 'i'
 
-        #abrimos el archivo txt y escribimos el texto extraido con el lector y al final lo cerramos(buena practica)
-        archivoN = open (nombre_doc,'a') 
-        archivoN.write(pagina.extract_text()) 
-        archivoN.close()
+            #abrimos el archivo txt y escribimos el texto extraido con el lector y al final lo cerramos(buena practica)
+            archivoN = open (nombre_doc,'a') 
+            archivoN.write(pagina.extract_text()) 
+            archivoN.close()
         
 x = 'juan'
 print ('Angelo estuvo aqui')
