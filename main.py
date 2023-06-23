a8ff512a8a8bb5efce4ce5b2862b49eef294ffc1
HandSoap03 <euceda.mary03@gmail.com>
2023-06-22 21:34:54 -0600

    Desastre 1 version 2

--- main.py
+++ main.py
@@ -45,8 +45,38 @@ def translate(archivo, source_lang, target_lang):
     return translated_text
 
 ####try2
+def translate1(file_path, source_lang, target_lang):
+    if file_path.lower().endswith(".txt"):  # check if file has .txt extension
+        with open(file_path, "r") as file:
+            lines = [line.strip() for line in file]  # use list comprehension to read lines
 
-def translate(archivo, source_lang, target_lang):
+        tokens = tokenizer.encode(lines, return_tensors="pt")  # encode lines as tokens
+    else:
+        print("Not a text file")
+        return ""
+
+    translated = model.generate(tokens, max_length=911, num_beams=4, early_stopping=True)  # generate translation
+    translated_text = tokenizer.decode(translated[0], skip_special_tokens=True)  # decode translation
+
+    return translated_text
+
+def translate2(archivo, source_lang, target_lang):
+    if archivo.endswith('.txt'):
+        with open(archivo, 'r') as f:
+            text = f.read()
+    else: 
+        print('Invalid file type')
+        return 
+
+    input_ids = tokenizer.encode(text, return_tensors='pt')
+    translated = model.generate(input_ids, max_length=512, num_beams=4, early_stopping=True)
+    translated_text = tokenizer.decode(translated[0], skip_special_tokens=True)
+
+    return translated_text
+
+
+
+def translate3(archivo, source_lang, target_lang):
     if (r"\.txt$", archivo):
         lineas = []  # Array para almacenar las líneas del archivo
         with open(archivo, 'r') as archivo:
@@ -73,13 +103,8 @@ def translate(archivo, source_lang, target_lang):
 
 while True:
     # Texto de entrada
-    input_text = str(input('Ingrese el texto que desea traducir (o escriba "Salir" para salir): '))
-    if input_text == 'Salir':
-        break
-    else:
-        translated_text = translate(input_text, 'en', 'es')
-        print("English:", input_text)
-        print("Spanish:", translated_text)
+    nombredoc = pdf_a_txt('Cinderella.pdf')
+    print(translate1(nombredoc, 'en', 'es'))
 
 
 # Funciones para ler un archivo y exportar un txt traducido
